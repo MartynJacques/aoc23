dd0babd57e307e405e1d839764c82511e4acd9bd
Martyn Jacques <martynjacques@gmail.com>
2023-12-14 11:56:45 +0000

    Day 14

---
+++ day14/part1.cpp
@@ -0,0 +1,87 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+std::vector<std::string> split(const std::string& target, char c) {
+  std::string temp;
+  std::stringstream stringstream{target};
+  std::vector<std::string> result;
+
+  while (std::getline(stringstream, temp, c)) {
+    result.push_back(temp);
+  }
+
+  return result;
+}
+
+std::vector<std::vector<char>> rotate(
+    std::vector<std::vector<char>> const& in) {
+  int rows = in.size();
+  int cols = in[0].size();
+
+  std::vector<std::vector<char>> rotated(cols, std::vector<char>(rows));
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      rotated[j][rows - 1 - i] = in[i][j];
+    }
+  }
+
+  return rotated;
+}
+
+void print_grid(std::vector<std::vector<char>> const& grid) {
+  for (auto const& row : grid) {
+    for (auto const& c : row) {
+      std::cout << c;
+    }
+    std::cout << '\n';
+  }
+}
+
+int main() {
+  std::ifstream infile("input.txt");
+  if (infile.fail()) {
+    std::cout << "Failed to open file\n";
+  }
+
+  std::string input_line{};
+  std::vector<std::vector<char>> grid{};
+  while (std::getline(infile, input_line)) {
+    std::vector<char> grid_row{};
+    for (int i = 0; i < input_line.size(); i++) {
+      grid_row.push_back(input_line.at(i));
+    }
+    grid.push_back(grid_row);
+  }
+
+  grid = rotate(grid);
+
+  for (int i = 0; i < grid.size(); i++) {
+    for (int j = grid.at(0).size() - 1; j >= 0; j--) {
+      if (grid.at(i).at(j) == 'O') {
+        // bubble this as far right as possible
+        int k = j;
+        while (k < grid.at(0).size() - 1 && grid.at(i).at(k + 1) != '#') {
+          // swap k and k + 1
+          char tmp = grid.at(i).at(k + 1);
+          grid[i][k + 1] = grid[i][k];
+          grid[i][k] = tmp;
+          k++;
+        }
+      }
+    }
+  }
+
+  int total_load = 0;
+  for (int i = 0; i < grid.size(); i++) {
+    for (int j = 0; j < grid.at(0).size(); j++) {
+      if (grid.at(i).at(j) == 'O') {
+        total_load += j + 1;
+      }
+    }
+  }
+
+  std::cout << total_load << '\n';
+}
